7fdd3b5e0de6154c8890922090d7c26ebcee6d54
Nandhu S <nandhus3860kvk@gmail.com>
2025-10-29 00:25:25 +0530

    app.py

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout
 from tensorflow.keras.callbacks import EarlyStopping
 from wordcloud import WordCloud
 import tensorflow as tf
+import os
 
 # ---------------------------------------------------
 # GPU / CPU DETECTION
@@ -236,8 +237,7 @@ def predict():
     pred = int(model.predict(vec)[0])
     return jsonify({'label': pred})
 
-# ---------------------------------------------------
-# Run App
-# ---------------------------------------------------
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
+
